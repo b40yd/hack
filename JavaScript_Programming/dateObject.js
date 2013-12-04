9ea1d7cf4730c24227dea6ca6a3fd26d2940962a
Qing Bao <wackOnline@users.noreply.github.com>
2013-12-04 10:42:49 +0800

    add format function

--- JavaScript_Programming/dateObject.js
+++ JavaScript_Programming/dateObject.js
@@ -112,11 +112,37 @@ dateObject.prototype.getNextMonth = function(times){
 
 Date.prototype.toString = function ()
 {
-	return this.getFullYear() + "/" + (this.getMonth()+1) + "/" + this.getDate();
+	return this.format("yyyy-MM-dd");
 };
 Date.prototype.toDateTimeString = function(){
-	return this.getFullYear() + "/" + (this.getMonth()+1) + "/" + this.getDate()+" "+this.getHours()+":"+this.getMinutes()+":"+this.getSeconds();
+	return this.format("yyyy-MM-dd hh:mm:ss");
 };
 Date.prototype.toTimeString = function(){
-	return this.getHours()+":"+this.getMinutes()+":"+this.getSeconds();
+	return this.format("hh:mm:ss");;
 }; 
+
+//(new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423   
+//(new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18   
+Date.prototype.format = function(fmt)   
+{ 
+	//author: meizz   
+	var o = {   
+			"M+" : this.getMonth()+1,                 //月份   
+			"d+" : this.getDate(),                    //日   
+			"h+" : this.getHours(),                   //小时   
+			"m+" : this.getMinutes(),                 //分   
+			"s+" : this.getSeconds(),                 //秒   
+			"q+" : Math.floor((this.getMonth()+3)/3), //季度   
+			"S"  : this.getMilliseconds()             //毫秒   
+	};   
+	if(/(y+)/.test(fmt))   
+		fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));   
+	for(var k in o){
+		if(new RegExp("("+ k +")").test(fmt)){
+			fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+		}
+	}
+		
+			   
+	return fmt;   
+} 

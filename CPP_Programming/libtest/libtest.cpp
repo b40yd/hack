fd381f2fa5fbb0f14ac694619c815a4780340a66
tag.bao <bb.qnyd@gmail.com>
2015-10-12 05:01:14 -0400

    test cpp

---
+++ CPP_Programming/libtest/libtest.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "libtest.h"
+
+void CTest::Test::doWork()
+{
+	printf("CTest::Test::doWork()");
+}
---
+++ CPP_Programming/libtest/libtest.h
@@ -0,0 +1,12 @@
+#ifndef _LIBTEST_H_
+#pragma once
+#define _LIBTEST_H_
+#endif
+namespace CTest
+{
+	class Test
+	{
+	public:
+		void doWork();
+	};
+}

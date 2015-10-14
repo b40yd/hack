1f6bd9500a3808a2cf49dc879d73876ae2f226c0
tag.bao <bb.qnyd@gmail.com>
2015-10-14 02:09:20 -0400

    test

--- C_Programming/pthread_test.c
+++ C_Programming/pthread_test.c
@@ -21,20 +21,20 @@ void* thread2(void*);
 
 int main(int argc, char** argv)
 {
-	printf("enter main\n");
-	pthread_t tid1, tid2;
-	int rc1=0, rc2=0;
-	rc2 = pthread_create(&tid2, NULL, thread2, NULL);
-	if(rc2 != 0)
-		printf("%s: %d\n",__func__, strerror(rc2));
+  printf("enter main\n");
+  pthread_t tid1, tid2;
+  int rc1=0, rc2=0;
+  rc2 = pthread_create(&tid2, NULL, thread2, NULL);
+  if(rc2 != 0)
+    printf("%s: %d\n",__func__, strerror(rc2));
 
-	rc1 = pthread_create(&tid1, NULL, thread1, &tid2);
-	if(rc1 != 0)
-		printf("%s: %d\n",__func__, strerror(rc1));
+  rc1 = pthread_create(&tid1, NULL, thread1, &tid2);
+  if(rc1 != 0)
+    printf("%s: %d\n",__func__, strerror(rc1));
 
-	pthread_cond_wait(&cond, &mutex);
-	printf("leave main\n");
-	exit(0);	
+  pthread_cond_wait(&cond, &mutex);
+  printf("leave main\n");
+  exit(0);	
 }
 
 /*
@@ -43,17 +43,17 @@ int main(int argc, char** argv)
  */
 void* thread1(void* arg)
 {
-	printf("enter thread1\n");
-	printf("this is thread1, g_Flag: %d, thread id is %u\n",g_Flag, (unsigned int)pthread_self());
-	pthread_mutex_lock(&mutex);
-	if(g_Flag == 2)
-		pthread_cond_signal(&cond);
-	g_Flag = 1;
-	printf("this is thread1, g_Flag: %d, thread id is %u\n",g_Flag, (unsigned int)pthread_self());
-	pthread_mutex_unlock(&mutex);
-	pthread_join(*(pthread_t*)arg, NULL);
-	printf("leave thread1\n");
-	pthread_exit(0);
+  printf("enter thread1\n");
+  printf("this is thread1, g_Flag: %d, thread id is %u\n",g_Flag, (unsigned int)pthread_self());
+  pthread_mutex_lock(&mutex);
+  if(g_Flag == 2)
+    pthread_cond_signal(&cond);
+  g_Flag = 1;
+  printf("this is thread1, g_Flag: %d, thread id is %u\n",g_Flag, (unsigned int)pthread_self());
+  pthread_mutex_unlock(&mutex);
+  pthread_join(*(pthread_t*)arg, NULL);
+  printf("leave thread1\n");
+  pthread_exit(0);
 }
 
 /*
@@ -62,14 +62,14 @@ void* thread1(void* arg)
  */
 void* thread2(void* arg)
 {
-	printf("enter thread2\n");
-	printf("this is thread2, g_Flag: %d, thread id is %u\n",g_Flag, (unsigned int)pthread_self());
-	pthread_mutex_lock(&mutex);
-	if(g_Flag == 1)
-		pthread_cond_signal(&cond);
-	g_Flag = 2;
-	printf("this is thread2, g_Flag: %d, thread id is %u\n",g_Flag, (unsigned int)pthread_self());
-	pthread_mutex_unlock(&mutex);
-	printf("leave thread2\n");
-	pthread_exit(0);
+  printf("enter thread2\n");
+  printf("this is thread2, g_Flag: %d, thread id is %u\n",g_Flag, (unsigned int)pthread_self());
+  pthread_mutex_lock(&mutex);
+  if(g_Flag == 1)
+    pthread_cond_signal(&cond);
+  g_Flag = 2;
+  printf("this is thread2, g_Flag: %d, thread id is %u\n",g_Flag, (unsigned int)pthread_self());
+  pthread_mutex_unlock(&mutex);
+  printf("leave thread2\n");
+  pthread_exit(0);
 }

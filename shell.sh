d002db658fc92ae71869a05a37eba7f63e46be88
7ym0n.q6e <7ym0n@users.noreply.github.com>
2016-09-13 10:54:47 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,2 +1,9 @@
 # 以时间排序文件
+```
 find . -type f -exec stat --format '%Y :%y %n' "{}" \; | grep -v cache | sort -nr | cut -d: -f2- | head
+```
+
+#基于时间排序
+```
+find . -type f -name "*.c" | xargs stat --format "%y %n" |sort -rn |head
+```
